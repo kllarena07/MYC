6d0b40763d4d644d67b79b6dee2ea2b7304d3e9b
Kieran Llarena <baymax709@gmail.com>
2024-02-10 18:51:09 -0500

    feat: created interface

--- frontend/content.js
+++ frontend/content.js
@@ -1,9 +1,52 @@
-const mainSection = document.getElementsByTagName("ytd-rich-item-renderer");
-const videos = document.getElementsByTagName("ytd-rich-grid-media");
-const sideMenu = document.getElementsByTagName("ytd-guide-section-renderer");
-const sideMenuLinks = document.getElementsByTagName("ytd-guide-entry-renderer");
-const sideRecommendedSection = document.getElementsByTagName(
-  "ytd-compact-video-renderer"
-  );
-const shortsMain = document.getElementsByTagName("ytd-thumbnail");
-const comments = document.getElementsByTagName("ytd-comments");
+const mountInterfaceToggle = (elem) => {
+  const btn = document.createElement("button")
+  btn.textContent = "Talk to Myc"
+  btn.style.width = "100%"
+  btn.addEventListener("click", () => {
+    const interface = document.getElementById('interface')
+    const isVisible = interface.style.display == "block"
+
+    isVisible ? interface.style.display = "none" : interface.style.display = "block"
+  })
+
+  const firstChild = elem.firstElementChild
+
+  elem.insertBefore(btn, firstChild)
+  console.log("Successfully mounted interface toggle")
+}
+
+const mountInterface = (elem) => {
+  const interface = document.createElement("section")
+  interface.style.width = "100%"
+  interface.style.height = "458.5px"
+  interface.id = "interface"
+  interface.style.display = "none"
+
+  const firstChild = elem.firstElementChild
+
+  elem.insertBefore(interface, firstChild)
+  console.log("Successfully mounted interface")
+}
+
+window.onload = () => {
+  let tries = 0;
+  console.log("Searching for #secondary-inner")
+  let secondaryInner = document.getElementById("secondary-inner")
+
+  const forceSelect = setInterval(() => {
+    if (!secondaryInner) {
+      secondaryInner = document.getElementById("secondary-inner")
+      console.log("#secondary-inner element not detected. Searching again. Current attempt:", tries)
+      ++tries
+    } else if (tries >= 5) {
+      console.log("Maximum number of tries exceeded. Ending search")
+      clearInterval(forceSelect)
+    } else {
+      clearInterval(forceSelect)
+      mountInterface(secondaryInner)
+      setTimeout(() => {
+        mountInterfaceToggle(secondaryInner)
+      }, 80)
+    }
+  }, 50)
+}
