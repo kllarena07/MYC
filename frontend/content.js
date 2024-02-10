f0e9b00f92695f9b9d75901b9e04e0f331a30613
Kieran Llarena <baymax709@gmail.com>
2024-02-10 16:49:28 -0500

    refactor :(

---
+++ frontend/content.js
@@ -0,0 +1,9 @@
+const mainSection = document.getElementsByTagName("ytd-rich-item-renderer");
+const videos = document.getElementsByTagName("ytd-rich-grid-media");
+const sideMenu = document.getElementsByTagName("ytd-guide-section-renderer");
+const sideMenuLinks = document.getElementsByTagName("ytd-guide-entry-renderer");
+const sideRecommendedSection = document.getElementsByTagName(
+  "ytd-compact-video-renderer"
+  );
+const shortsMain = document.getElementsByTagName("ytd-thumbnail");
+const comments = document.getElementsByTagName("ytd-comments");
