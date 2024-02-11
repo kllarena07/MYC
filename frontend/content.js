2485c6acfce3e163bedd5bdadca6077ccc363bd0
Kieran Llarena <baymax709@gmail.com>
2024-02-10 23:43:32 -0500

    refactor: created global state

--- frontend/content.js
+++ frontend/content.js
@@ -1,30 +1,87 @@
-const mountInterfaceToggle = (elem) => {
+const attention = {
+  animation: [
+    { transform: 'rotate(0deg)' },
+    { transform: 'rotate(-2deg)' },
+    { transform: 'rotate(2deg)' },
+    { transform: 'rotate(0deg)' }
+  ],
+  options: {
+    duration: 1000,
+    iterations: 1,
+    easing: "ease"
+  }
+}
+
+const grow = {
+  animation: [
+    { height: '0' },
+    { height: '480px' }
+  ],
+  options: {
+    duration: 800,
+    iterations: 1,
+    fill: 'forwards'
+  }
+}
+
+const shrink = {
+  animation: [
+    { height: '480px' },
+    { height: '0' }
+  ],
+  options: {
+    duration: 800,
+    iterations: 1,
+    fill: 'forwards'
+  }
+}
+
+let buttonToggled = false
+let stores = {
+  dropdown: undefined,
+  video: undefined,
+  btn: undefined
+}
+const toggleDropdown = () => {
+  if(!buttonToggled) {
+    stores.dropdown.animate(grow.animation, grow.options)
+    stores.video.pause()
+    stores.btn.textContent = "Close Conversation"
+    buttonToggled = true
+  } else {
+    stores.dropdown.animate(shrink.animation, shrink.options)
+    stores.video.play()
+    stores.btn.textContent = "Talk to Myc"
+    buttonToggled = false
+  }
+}
+
+const mountDropdownToggle = (elem) => {
   const btn = document.createElement("button")
   btn.textContent = "Talk to Myc"
   btn.style.width = "100%"
-  btn.addEventListener("click", () => {
-    const interface = document.getElementById('interface')
-    const isVisible = interface.style.display == "block"
-
-    isVisible ? interface.style.display = "none" : interface.style.display = "block"
-  })
+  btn.style.transition = "box-shadow 200ms ease"
+  btn.id = "toggle-interface"
+  btn.addEventListener("click", toggleDropdown)
 
   const firstChild = elem.firstElementChild
 
   elem.insertBefore(btn, firstChild)
+  stores.btn = btn
   console.log("Successfully mounted interface toggle")
 }
 
-const mountInterface = (elem) => {
-  const interface = document.createElement("section")
-  interface.style.width = "100%"
-  interface.style.height = "458.5px"
-  interface.id = "interface"
-  interface.style.display = "none"
+const mountDropdown = (elem) => {
+  const dropdown = document.createElement("section")
+  dropdown.style.width = "100%"
+  dropdown.style.backgroundColor = "blue"
+  dropdown.style.height = "0"
+  dropdown.id = "dropdown"
 
   const firstChild = elem.firstElementChild
 
-  elem.insertBefore(interface, firstChild)
+  elem.insertBefore(dropdown, firstChild)
+  stores.dropdown = dropdown
   console.log("Successfully mounted interface")
 }
 
@@ -43,10 +100,25 @@ window.onload = () => {
       clearInterval(forceSelect)
     } else {
       clearInterval(forceSelect)
-      mountInterface(secondaryInner)
+      
+      mountDropdown(secondaryInner)
       setTimeout(() => {
-        mountInterfaceToggle(secondaryInner)
-      }, 80)
+        mountDropdownToggle(secondaryInner)
+      }, 500)
+
+      stores.video = document.querySelector('.video-stream.html5-main-video')
+      stores.video.addEventListener("pause", () => {
+        if(!buttonToggled) {
+          stores.btn.animate(attention.animation, attention.options);
+        }
+      })
+
+      console.log(stores)
     }
-  }, 50)
+  }, 800)
 }
+
+// when button is clicked
+// 1. video state changes from playing to pause
+// 2. time instance is recorded
+// 3. 
